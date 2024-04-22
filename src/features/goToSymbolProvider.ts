5d4b2d30f729e8a5d03414caa015551b465cdd51
Paulo Moura <pmoura@logtalk.org>
2024-04-22 14:07:32 +0100

    Go to symbol performance optimization

--- src/features/goToSymbolProvider.ts
+++ src/features/goToSymbolProvider.ts
@@ -23,25 +23,24 @@ export class LogtalkDocumentSymbolProvider implements DocumentSymbolProvider {
       let ppro = /(?:\s*\:- protected\()(\w+[/]\d+)/;
       let ppri = /(?:\s*\:- private\()(\w+[/]\d+)/;
 
+      let found;
+
       for (var i = 0; i < doc.lineCount; i++) {
         var line = doc.lineAt(i);
         if (line.text.startsWith(":- object(")) {
-          const found = line.text.match(ro);
+          found = line.text.match(ro);
           symbols.push(new SymbolInformation(found[1], SymbolKind.Object, "object", new Location(doc.uri, line.range)))
         } else if (line.text.startsWith(":- protocol(")) {
-          const found = line.text.match(rp);
+          found = line.text.match(rp);
           symbols.push(new SymbolInformation(found[1], SymbolKind.Interface, "protocol", new Location(doc.uri, line.range)))
         } else if (line.text.startsWith(":- category(")) {
-          const found = line.text.match(rc);
+          found = line.text.match(rc);
           symbols.push(new SymbolInformation(found[1], SymbolKind.Struct, "category", new Location(doc.uri, line.range)))
-        } else if (line.text.match(ppub)) {
-          const found = line.text.match(ppub);
+        } else if (found = line.text.match(ppub)) {
           symbols.push(new SymbolInformation(found[1], SymbolKind.Function, "public predicate", new Location(doc.uri, line.range)))
-        } else if (line.text.match(ppro)) {
-          const found = line.text.match(ppro);
+        } else if (found = line.text.match(ppro)) {
           symbols.push(new SymbolInformation(found[1], SymbolKind.Function, "protected predicate", new Location(doc.uri, line.range)))
-        } else if (line.text.match(ppri)) {
-          const found = line.text.match(ppri);
+        } else if (found = line.text.match(ppri)) {
           symbols.push(new SymbolInformation(found[1], SymbolKind.Function, "private predicate", new Location(doc.uri, line.range)))
         }
       }
