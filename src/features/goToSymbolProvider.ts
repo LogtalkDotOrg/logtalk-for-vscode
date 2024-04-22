0a2fad41258db656db9dc543071ef43a933ac0ec
Paulo Moura <pmoura@logtalk.org>
2024-04-22 12:37:34 +0100

    Allow using go to symbol to navigate to predicate declarations

--- src/features/goToSymbolProvider.ts
+++ src/features/goToSymbolProvider.ts
@@ -19,6 +19,10 @@ export class LogtalkDocumentSymbolProvider implements DocumentSymbolProvider {
       let rp = /(?:\:- protocol\()([^(),.]+(\(.*\))?)/;
       let rc = /(?:\:- category\()([^(),.]+(\(.*\))?)/;
 
+      let ppub = /(?:\s*\:- public\()(\w+[/]\d+)/;
+      let ppro = /(?:\s*\:- protected\()(\w+[/]\d+)/;
+      let ppri = /(?:\s*\:- private\()(\w+[/]\d+)/;
+
       for (var i = 0; i < doc.lineCount; i++) {
         var line = doc.lineAt(i);
         if (line.text.startsWith(":- object(")) {
@@ -29,7 +33,16 @@ export class LogtalkDocumentSymbolProvider implements DocumentSymbolProvider {
           symbols.push(new SymbolInformation(found[1], SymbolKind.Interface, "protocol", new Location(doc.uri, line.range)))
         } else if (line.text.startsWith(":- category(")) {
           const found = line.text.match(rc);
-          symbols.push(new SymbolInformation(found[1], SymbolKind.Package, "category", new Location(doc.uri, line.range)))
+          symbols.push(new SymbolInformation(found[1], SymbolKind.Struct, "category", new Location(doc.uri, line.range)))
+        } else if (line.text.match(ppub)) {
+          const found = line.text.match(ppub);
+          symbols.push(new SymbolInformation(found[1], SymbolKind.Function, "public predicate", new Location(doc.uri, line.range)))
+        } else if (line.text.match(ppro)) {
+          const found = line.text.match(ppro);
+          symbols.push(new SymbolInformation(found[1], SymbolKind.Function, "protected predicate", new Location(doc.uri, line.range)))
+        } else if (line.text.match(ppri)) {
+          const found = line.text.match(ppri);
+          symbols.push(new SymbolInformation(found[1], SymbolKind.Function, "private predicate", new Location(doc.uri, line.range)))
         }
       }
 
