cbcb56f130a63d1f9c2c3b55ff9c0927ffd32119
Paulo Moura <pmoura@logtalk.org>
2025-09-02 14:40:29 +0100

    Remove diagnostic after applying quick fix

--- src/extension.ts
+++ src/extension.ts
@@ -37,7 +37,6 @@ import { LogtalkTypeHierarchyProvider } from "./features/typeHierarchyProvider";
 import { LogtalkMetricsCodeLensProvider } from "./features/metricsCodeLensProvider";
 import { LogtalkTestsCodeLensProvider } from "./features/testsCodeLensProvider";
 import { LogtalkChatParticipant } from "./features/logtalkChatParticipant";
-import { LogtalkCodeActionsProvider } from "./features/codeActionsProvider";
 import { getLogger } from "./utils/logger";
 
 const DEBUG = 1;
@@ -246,39 +245,41 @@ export function activate(context: ExtensionContext) {
       commands.executeCommand('setContext', 'logtalk.debuggingEnabled', logtalkDebuggingEnabled);
     }},
     // Workspace commands
-    { command: "logtalk.create.project",          callback: ()   => LogtalkTerminal.createProject()},
-    { command: "logtalk.load.project",            callback: uri  => LogtalkTerminal.loadProject(uri, linter)},
-    { command: "logtalk.open",                    callback: ()   => LogtalkTerminal.openLogtalk()},
-    { command: "logtalk.rscan.deadCode",          callback: uri  => LogtalkTerminal.rscanForDeadCode(uri, deadCodeScanner)},
-    { command: "logtalk.rgenerate.documentation", callback: uri  => LogtalkTerminal.rgenDocumentation(uri, documentationLinter)},
-    { command: "logtalk.rgenerate.diagrams",      callback: uri  => LogtalkTerminal.rgenDiagrams(uri)},
-    { command: "logtalk.run.testers",             callback: uri  => LogtalkTerminal.runTesters(uri)},
-    { command: "logtalk.run.doclets",             callback: uri  => LogtalkTerminal.runDoclets(uri)},
+    { command: "logtalk.create.project",            callback: ()   => LogtalkTerminal.createProject()},
+    { command: "logtalk.load.project",              callback: uri  => LogtalkTerminal.loadProject(uri, linter)},
+    { command: "logtalk.open",                      callback: ()   => LogtalkTerminal.openLogtalk()},
+    { command: "logtalk.rscan.deadCode",            callback: uri  => LogtalkTerminal.rscanForDeadCode(uri, deadCodeScanner)},
+    { command: "logtalk.rgenerate.documentation",   callback: uri  => LogtalkTerminal.rgenDocumentation(uri, documentationLinter)},
+    { command: "logtalk.rgenerate.diagrams",        callback: uri  => LogtalkTerminal.rgenDiagrams(uri)},
+    { command: "logtalk.run.testers",               callback: uri  => LogtalkTerminal.runTesters(uri)},
+    { command: "logtalk.run.doclets",               callback: uri  => LogtalkTerminal.runDoclets(uri)},
     // Directory and file commands
-    { command: "logtalk.load.directory",          callback: uri  => LogtalkTerminal.loadDirectory(uri, linter)},
-    { command: "logtalk.load.file",               callback: uri  => LogtalkTerminal.loadFile(uri, linter)},
-    { command: "logtalk.make.reload",             callback: uri  => LogtalkTerminal.makeReload(uri, linter)},
-    { command: "logtalk.make.optimal",            callback: uri  => LogtalkTerminal.makeOptimal(uri, linter)},
-    { command: "logtalk.make.normal",             callback: uri  => LogtalkTerminal.makeNormal(uri, linter)},
-    { command: "logtalk.make.debug",              callback: uri  => LogtalkTerminal.makeDebug(uri, linter)},
-    { command: "logtalk.make.check",              callback: uri  => LogtalkTerminal.makeCheck(uri, linter)},
-    { command: "logtalk.make.circular",           callback: uri  => LogtalkTerminal.makeCircular(uri, linter)},
-    { command: "logtalk.make.clean",              callback: uri  => LogtalkTerminal.makeClean(uri, linter)},
-    { command: "logtalk.make.caches",             callback: uri  => LogtalkTerminal.makeCaches(uri, linter)},
-    { command: "logtalk.run.tests",               callback: uri  => LogtalkTerminal.runTests(uri, linter, testsReporter)},
-    { command: "logtalk.run.test",                callback: (uri, object, test) => LogtalkTerminal.runTest(uri, object, test, linter, testsReporter)},
-    { command: "logtalk.run.doclet",              callback: uri  => LogtalkTerminal.runDoclet(uri, linter)},
-    { command: "logtalk.scan.deadCode",           callback: uri  => LogtalkTerminal.scanForDeadCode(uri, deadCodeScanner)},
-    { command: "logtalk.generate.documentation",  callback: uri  => LogtalkTerminal.genDocumentation(uri, documentationLinter)},
-    { command: "logtalk.generate.diagrams",       callback: uri  => LogtalkTerminal.genDiagrams(uri)},
-    { command: "logtalk.open.parentFile",         callback: uri  => LogtalkTerminal.openParentFile(uri)},
-    { command: "logtalk.compute.metrics",         callback: uri  => LogtalkTerminal.computeMetrics(uri)},
+    { command: "logtalk.load.directory",            callback: uri  => LogtalkTerminal.loadDirectory(uri, linter)},
+    { command: "logtalk.load.file",                 callback: uri  => LogtalkTerminal.loadFile(uri, linter)},
+    { command: "logtalk.make.reload",               callback: uri  => LogtalkTerminal.makeReload(uri, linter)},
+    { command: "logtalk.make.optimal",              callback: uri  => LogtalkTerminal.makeOptimal(uri, linter)},
+    { command: "logtalk.make.normal",               callback: uri  => LogtalkTerminal.makeNormal(uri, linter)},
+    { command: "logtalk.make.debug",                callback: uri  => LogtalkTerminal.makeDebug(uri, linter)},
+    { command: "logtalk.make.check",                callback: uri  => LogtalkTerminal.makeCheck(uri, linter)},
+    { command: "logtalk.make.circular",             callback: uri  => LogtalkTerminal.makeCircular(uri, linter)},
+    { command: "logtalk.make.clean",                callback: uri  => LogtalkTerminal.makeClean(uri, linter)},
+    { command: "logtalk.make.caches",               callback: uri  => LogtalkTerminal.makeCaches(uri, linter)},
+    { command: "logtalk.run.tests",                 callback: uri  => LogtalkTerminal.runTests(uri, linter, testsReporter)},
+    { command: "logtalk.run.test",                  callback: (uri, object, test) => LogtalkTerminal.runTest(uri, object, test, linter, testsReporter)},
+    { command: "logtalk.run.doclet",                callback: uri  => LogtalkTerminal.runDoclet(uri, linter)},
+    { command: "logtalk.scan.deadCode",             callback: uri  => LogtalkTerminal.scanForDeadCode(uri, deadCodeScanner)},
+    { command: "logtalk.generate.documentation",    callback: uri  => LogtalkTerminal.genDocumentation(uri, documentationLinter)},
+    { command: "logtalk.generate.diagrams",         callback: uri  => LogtalkTerminal.genDiagrams(uri)},
+    { command: "logtalk.open.parentFile",           callback: uri  => LogtalkTerminal.openParentFile(uri)},
+    { command: "logtalk.compute.metrics",           callback: uri  => LogtalkTerminal.computeMetrics(uri)},
     // CodeLens commands
-    { command: "logtalk.toggle.codeLens",         callback: uri  => LogtalkTerminal.toggleCodeLens(uri)},
+    { command: "logtalk.toggle.codeLens",           callback: uri  => LogtalkTerminal.toggleCodeLens(uri)},
     // Jupyter commands
-    { command: "logtalk.open.notebook",           callback: uri  => LogtalkJupyter.openAsNotebook(uri)},
-    { command: "logtalk.open.paired.notebook",    callback: uri  => LogtalkJupyter.openAsPairedNotebook(uri)},
-    { command: "logtalk.sync.notebook",           callback: uri  => LogtalkJupyter.syncNotebook(uri)}
+    { command: "logtalk.open.notebook",             callback: uri  => LogtalkJupyter.openAsNotebook(uri)},
+    { command: "logtalk.open.paired.notebook",      callback: uri  => LogtalkJupyter.openAsPairedNotebook(uri)},
+    { command: "logtalk.sync.notebook",             callback: uri  => LogtalkJupyter.syncNotebook(uri)},
+    // Diagnostic commands
+    { command: "logtalk.linter.update.diagnostics", callback: (uri, diagnostic) => linter.updateDiagnostics(uri, diagnostic)}
   ];
 
   logtalkCommands.map(command => {
@@ -362,7 +363,7 @@ export function activate(context: ExtensionContext) {
     languages.registerCodeLensProvider(LOGTALK_MODE, new LogtalkMetricsCodeLensProvider())
   );
   context.subscriptions.push(
-    languages.registerCodeActionsProvider(LOGTALK_MODE, new LogtalkCodeActionsProvider())
+    languages.registerCodeActionsProvider(LOGTALK_MODE, linter)
   );
   context.subscriptions.push(LogtalkTerminal.init(context));
 }
--- src/features/logtalkLinter.ts
+++ src/features/logtalkLinter.ts
@@ -4,7 +4,9 @@ import {
   CancellationToken,
   CodeActionContext,
   CodeActionProvider,
-  Command,
+  CodeAction,
+  CodeActionKind,
+  WorkspaceEdit,
   Diagnostic,
   DiagnosticCollection,
   DiagnosticSeverity,
@@ -34,14 +36,180 @@ export default class LogtalkLinter implements CodeActionProvider {
     this.loadConfiguration();
   }
 
-  provideCodeActions(
-    document: TextDocument,
-    range: Range,
-    context: CodeActionContext,
-    token: CancellationToken
-  ): Command[] | Thenable<Command[]> {
-    let codeActions: Command[] = [];
-    return codeActions;
+  public async provideCodeActions(
+    document: TextDocument, range: Range | Selection,
+    context: CodeActionContext, token: CancellationToken):
+    Promise<CodeAction[]> {
+      const actions: CodeAction[] = [];
+      // Iterate through diagnostics (errors/warnings) in the current context
+      for (const diagnostic of context.diagnostics) {
+        // Check if this diagnostic has an associated quick fix
+        if (this.canFix(diagnostic)) {
+          const action = this.createQuickFix(document, diagnostic);
+          if (action) {
+            actions.push(action);
+          }
+        }
+      }  
+      return actions;
+    }
+
+  private canFix(diagnostic: Diagnostic): boolean {
+    if (diagnostic.message.includes('Redundant entity qualification in predicate directive argument:')) {
+      return true;
+    } else if (diagnostic.message.includes('Duplicated clause:')) {
+      return true;
+    } else if (diagnostic.message.includes('Duplicated directive:')) {
+      return true;
+    } else if (diagnostic.message.includes('Duplicated grammar rule:')) {
+      return true;
+    } else if (diagnostic.message.includes('Missing scope directive for predicate:')) {
+      return true;
+    } else if (diagnostic.message.includes('Missing scope directive for non-terminal:')) {
+      return true;
+    } else if (diagnostic.message.includes('Missing dynamic/1 directive for predicate:')) {
+      return true;
+    } else if (diagnostic.message.includes('Missing multifile/1 directive for predicate:')) {
+      return true;
+    } else if (diagnostic.message.includes('Missing multifile/1 directive for non-terminal:')) {
+      return true;
+    } else if (diagnostic.message.includes('The encoding/1 directive is ignored')) {
+      return true;
+    } else if (diagnostic.message.includes('Missing meta_predicate/1 directive for predicate:')) {
+      return true;
+    } else if (diagnostic.message.includes('Missing meta_non_terminal/1 directive for non-terminal:')) {
+      return true;
+    }
+    return false;
+  }
+
+  private createQuickFix(document: TextDocument, diagnostic: Diagnostic): CodeAction | null {
+    // Create the edit that will fix the issue
+    const edit = new WorkspaceEdit();
+    let action: CodeAction;
+
+    if (diagnostic.message.includes('Redundant entity qualification in predicate directive argument:')) {
+      // Remove the redundant entity qualification
+      action = new CodeAction(
+        'Fix redundant entity qualification',
+        CodeActionKind.QuickFix
+      );
+      const entityQualification = diagnostic.message.match(/Redundant entity qualification in predicate directive argument: (.+::).+/);
+      const match = document.getText(diagnostic.range).match(entityQualification[1]);
+      const deleteRange = new Range(
+        diagnostic.range.start.line,
+        match.index,
+        diagnostic.range.end.line,
+        match.index + match[0].length
+      );
+      edit.delete(document.uri, deleteRange);
+    } else if (diagnostic.message.includes('Duplicated clause:')) {
+      // Remove the duplicated clause
+      action = new CodeAction(
+        'Delete duplicated clause',
+        CodeActionKind.QuickFix
+      );
+      edit.delete(document.uri, diagnostic.range);
+    } else if (diagnostic.message.includes('Duplicated directive:')) {
+      // Remove the duplicated directive
+      action = new CodeAction(
+        'Delete duplicated directive',
+        CodeActionKind.QuickFix
+      );
+      edit.delete(document.uri, diagnostic.range);
+    } else if (diagnostic.message.includes('Duplicated grammar rule:')) {
+      // Remove the duplicated grammar rule
+      action = new CodeAction(
+        'Delete duplicated grammar rule',
+        CodeActionKind.QuickFix
+      );
+      edit.delete(document.uri, diagnostic.range);
+    } else if (diagnostic.message.includes('Missing scope directive for predicate:')) {
+      // Add missing scope directive
+      action = new CodeAction(
+        'Add missing public/1 directive',
+        CodeActionKind.QuickFix
+      );
+      const predicateIndicator = diagnostic.message.match(/Missing scope directive for predicate: (.+\/\d+)/);
+      const indent = document.getText(diagnostic.range).match(/(\s*)/);
+      edit.insert(document.uri, diagnostic.range.start, indent[1] + ':- public(' + predicateIndicator[1] + ').\n');
+    } else if (diagnostic.message.includes('Missing scope directive for non-terminal:')) {
+      // Add missing scope directive
+      action = new CodeAction(
+        'Add missing public/1 directive',
+        CodeActionKind.QuickFix
+      );
+      const nonTerminalIndicator = diagnostic.message.match(/Missing scope directive for non-terminal: (.+\/\/\d+)/);
+      const indent = document.getText(diagnostic.range).match(/(\s*)/);
+      edit.insert(document.uri, diagnostic.range.start, indent[1] + ':- public(' + nonTerminalIndicator[1] + ').\n');
+    } else if (diagnostic.message.includes('Missing dynamic/1 directive for predicate:')) {
+      // Add missing scope directive
+      action = new CodeAction(
+        'Add missing dynamic/1 directive',
+        CodeActionKind.QuickFix
+      );
+      const predicateIndicator = diagnostic.message.match(/Missing dynamic\/1 directive for predicate: (.+\/\d+)/);
+      const indent = document.getText(diagnostic.range).match(/(\s*)/);
+      edit.insert(document.uri, diagnostic.range.start, indent[1] + ':- dynamic(' + predicateIndicator[1] + ').\n');
+    } else if (diagnostic.message.includes('Missing multifile/1 directive for predicate:')) {
+      // Add missing scope directive
+      action = new CodeAction(
+        'Add missing multifile/1 directive',
+        CodeActionKind.QuickFix
+      );
+      const predicateIndicator = diagnostic.message.match(/Missing multifile\/1 directive for predicate: (.+\/\d+)/);
+      const indent = document.getText(diagnostic.range).match(/(\s*)/);
+      edit.insert(document.uri, diagnostic.range.start, indent[1] + ':- multifile(' + predicateIndicator[1] + ').\n');
+    } else if (diagnostic.message.includes('Missing multifile/1 directive for non-terminal:')) {
+      // Add missing scope directive
+      action = new CodeAction(
+        'Add missing multifile/1 directive',
+        CodeActionKind.QuickFix
+      );
+      const nonTerminalIndicator = diagnostic.message.match(/Missing multifile\/1 directive for non-terminal: (.+\/\/\d+)/);
+      const indent = document.getText(diagnostic.range).match(/(\s*)/);
+      edit.insert(document.uri, diagnostic.range.start, indent[1] + ':- multifile(' + nonTerminalIndicator[1] + ').\n');
+    } else if (diagnostic.message.includes('The encoding/1 directive is ignored')) {
+      // Move encoding/1 directive to the first line
+      action = new CodeAction(
+        'Move encoding/1 directive to the first line',
+        CodeActionKind.QuickFix
+      );
+      const text = document.getText(diagnostic.range).trim();
+      edit.delete(document.uri, diagnostic.range);
+      edit.insert(document.uri, new Position(0, 0), text + '\n');
+    } else if (diagnostic.message.includes('Missing meta_predicate/1 directive for predicate:')) {
+      // Add missing meta_predicate/1 directive
+      action = new CodeAction(
+        'Add missing meta_predicate/1 directive',
+        CodeActionKind.QuickFix
+      );
+      const predicateIndicator = diagnostic.message.match(/Missing meta_predicate\/1 directive for predicate: (.+)\/(\d+)/);
+      const stars = Array(predicateIndicator[2]).fill('*').join(',');
+      const indent = document.getText(diagnostic.range).match(/(\s*)/);
+      edit.insert(document.uri, diagnostic.range.start, indent[1] + ':- meta_predicate(' + predicateIndicator[1] + '(' + stars+ ')).\n');
+    } else if (diagnostic.message.includes('Missing meta_non_terminal/1 directive for non-terminal:')) {
+      // Add missing meta_non_terminal/1 directive
+      action = new CodeAction(
+        'Add missing meta_non_terminal/1 directive',
+        CodeActionKind.QuickFix
+      );
+      const nonTerminalIndicator = diagnostic.message.match(/Missing meta_non_terminal\/1 directive for non-terminal: (.+)\/\/(\d+)/);
+      const stars = Array(nonTerminalIndicator[2]).fill('*').join(',');
+      const indent = document.getText(diagnostic.range).match(/(\s*)/);
+      edit.insert(document.uri, diagnostic.range.start, indent[1] + ':- meta_non_terminal(' + nonTerminalIndicator[1] + '(' + stars + ')).\n');
+    }
+
+    action.edit = edit;
+    // Associate this action with the specific diagnostic
+    action.diagnostics = [diagnostic];
+    action.command = {
+      title: 'Logtalk Linter',
+      command: 'logtalk.linter.update.diagnostics',
+      arguments: [document.uri, diagnostic]
+    };
+
+    return action;
   }
 
   private parseIssue(issue: string) {
@@ -123,6 +291,21 @@ export default class LogtalkLinter implements CodeActionProvider {
       // Clear the diagnostic hash as we're starting a new compilation
       this.diagnosticHash = [];
     }
+  }
+
+  public updateDiagnostics(uri: Uri, diagnosticToRemove: Diagnostic) {
+    const existingDiagnostics = this.diagnosticCollection.get(uri) || [];
+    const filteredDiagnostics = existingDiagnostics.filter(
+      diagnostic => !this.areDiagnosticsEqual(diagnostic, diagnosticToRemove)
+    );
+    this.diagnosticCollection.set(uri, filteredDiagnostics);
+  }
+  
+  private areDiagnosticsEqual(a: Diagnostic, b: Diagnostic): boolean {
+    return a.range.isEqual(b.range) && 
+           a.message === b.message && 
+           a.severity === b.severity &&
+           a.code === b.code;
   }
 
   private removeDuplicateDiagnostics(diagnostics: Diagnostic[]): Diagnostic[] {
